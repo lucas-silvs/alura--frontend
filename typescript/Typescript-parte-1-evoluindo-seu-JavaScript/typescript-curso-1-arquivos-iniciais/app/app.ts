e557938750c22b8cdd257df69a80ab68018e064a
Lucas Santos <lucas.parkl@outlook.com>
2023-07-02 12:42:31 -0300

    fim do modulo 2 - TypeScript parte 1: evoluindo seu JavaScript: - convertendo o projeto para Typescript - procedimento de instalação do Typescript - ajustando as configurações de regras do Typescript no tsconfig.json

--- typescript/Typescript-parte-1-evoluindo-seu-JavaScript/typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript/Typescript-parte-1-evoluindo-seu-JavaScript/typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,4 +1,8 @@
 import { Negociacao } from "./models/negociacao.js";
+
+
 const negociacao = new Negociacao(new Date(), 1200, 100);
+
 console.log(`Negociação antes da alterção: ${negociacao}`);
+
 alert(`teste`);
--- typescript/Typescript-parte-1-evoluindo-seu-JavaScript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript/Typescript-parte-1-evoluindo-seu-JavaScript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,22 +1,32 @@
 export class Negociacao {
+    private _data: Date;
+    private _quantidade;
+    private _valor;
+
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
+
     // cria getters e setters
     get data() {
         return this._data;
     }
+    
+
     get quantidade() {
         return this._quantidade;
     }
+
     get valor() {
         return this._valor;
-    }
+    } 
+
     get volume() {
         return this._quantidade * this._valor;
     }
+
     toString() {
         return `
             Data: ${this._data}
@@ -25,4 +35,5 @@ export class Negociacao {
             Volume: ${this.volume}
         `;
     }
+
 }
