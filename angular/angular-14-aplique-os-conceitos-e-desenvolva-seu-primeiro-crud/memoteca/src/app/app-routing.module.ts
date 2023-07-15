2be9cff04842f4dd964cd233088618a37d1a05d8
Lucas Santos <lucas.parkl@outlook.com>
2023-07-15 20:41:25 -0300

    fim do modulo 3: Angular 14 - aplique os conceitos e desenvolva seu primeiro CRUD: - utilizando RouteModule para redirecionar para paginas de cadastro e listagens de Pensamentos - configurando rotas para cada página - utlizando a propriedade RouteLink para adicionar as ações de rotas em botoes

--- angular/angular-14-aplique-os-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/app-routing.module.ts
+++ angular/angular-14-aplique-os-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentoComponent
+  },
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentoComponent
+  },
+  {
+    path: '',
+    redirectTo: "listarPensamento",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
