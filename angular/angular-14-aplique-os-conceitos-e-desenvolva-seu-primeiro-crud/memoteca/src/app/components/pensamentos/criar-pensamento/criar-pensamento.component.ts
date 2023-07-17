6e39e0a32833bf7f1ab15b4fcd395e8339f4d86b
Lucas Santos <lucas.parkl@outlook.com>
2023-07-17 20:27:06 -0300

    fim do modulo 1: Angular 14 - evoluindo a aplicação: - criando formulário reativo utilizando FormBuilder - associando o formulário reativo ao form html com a diretiva FormGroup - associando campos do formulário reativo com os campos do form html com a dretiva formControlName - acessando valores dos campos dos formulários reativos

--- angular/angular-14-aplique-os-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ angular/angular-14-aplique-os-conceitos-e-desenvolva-seu-primeiro-crud/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento-interface';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,22 +11,24 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento: Pensamento = {
-    conteudo: "",
-    autoria: "",
-    modelo: ""
-  };
+  formulario!: FormGroup;
 
   constructor(
     private pensamentoService: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
     ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ["Formulário Reativo"],
+      autoria: ["teste autoria"],
+      modelo: ["modelo1"]
+    });
   }
 
   criarPensamento() {
-    this.pensamentoService.cadastrarPensamento(this.pensamento).subscribe(() => {
+    this.pensamentoService.cadastrarPensamento(this.formulario.value).subscribe(() => {
     });
   }
 
